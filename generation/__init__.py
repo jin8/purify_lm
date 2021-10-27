cb334c9a060b882c1d0cfee83149c571398f203c
Minseon Kim <minseonkim@kaist.ac.kr>
2021-10-27 14:12:33 +0900

    minseon_

--- generation/__init__.py
+++ generation/__init__.py
@@ -1,50 +1,23 @@
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-from .generation import gpt2, gpt3, pplm, dexperts, dexperts_gpt3, contrastive_gpt2
+from .generation import gpt2, gpt3, pplm, dexperts, dexperts_gpt3, contrastive_gpt2, style_gpt2, cont, mapping, att, attadd
 from .gpt2_generation import GPT2Generation
 from .dexperts_generation import DExpertsGeneration
 from .dexperts_gpt3_generation import DExpertsGPT3Generation
 from .pplm_generation import PPLMGeneration
 from .contrastive_generation import ContrastiveGeneration
-
-__all__ = [
-    'gpt2',
-    'gpt3',
-    'pplm',
-    'dexperts',
-    'dexperts_gpt3',
-    'contrastive_gpt2'
-]
-=======
-from .generation import gpt2, gpt3, pplm, dexperts, dexperts_gpt3, cont
-=======
-from .generation import gpt2, gpt3, pplm, dexperts, dexperts_gpt3, cont, mapping, att, attadd
->>>>>>> Stashed changes
-from .gpt2_generation import GPT2Generation
-from .dexperts_generation import DExpertsGeneration
-from .dexperts_gpt3_generation import DExpertsGPT3Generation
-from .pplm_generation import PPLMGeneration
-from .cont_generation import ContGeneration
-<<<<<<< Updated upstream
-=======
+from .style_transfer_generation import StyleGPT2Generation
 from .mapping_generation import MappingGeneration
 from .att_generation import AttGeneration
 from .att_add_generation import AttAddGeneration
->>>>>>> Stashed changes
 __all__ = [
     'gpt2',
     'gpt3',
     'pplm',
     'dexperts',
     'dexperts_gpt3',
-<<<<<<< Updated upstream
-    'cont'
-]
->>>>>>> Stashed changes
-=======
+    'contrastive_gpt2',
+    'style_gpt2'
     'cont',
     'mapping',
     'att',
     'attadd'
 ]
->>>>>>> Stashed changes
